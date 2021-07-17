155a39d5ebe8200858f26a3593f46791f7daff7c
Park Yong Ha <yonghapk@gmail.com>
2021-07-17 23:57:00 +0900

    ADD module : html,watch,broswersync,nodemon

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+app.set('views', '${ __dirname }/dist/');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use('/', express.static(`${__dirname}/dist/`));
+app.get('/', (req, res) => {
+	res.render('index', {});
+});
+var server = app.listen(8005, () => {
+	console.log('Express listening on port : ' + server.address().port);
+});
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,39 +1,73 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var nodemon = require('gulp-nodemon');
+var browserSync = require('browser-sync');
 
 // origin source
 var PATH = {
-	ASSETS : {
-		FONTS : './html/assets/fonts',
-		IMAGES : './html/assets/images',
-		STYLE : './html/assets/scss'
+	HTML: './workspace/html',
+	ASSETS: {
+		FONTS: './workspace/assets/fonts',
+		IMAGES: './workspace/assets/images',
+		STYLE: './workspace/assets/scss'
 	}
 }
 
 // dist
 var DEST_PATH = {
-	ASSETS : {
-		FONTS : './dist/assets/fonts',
-		IMAGES : './dist/assets/images',
-		STYLE : './dist/assets/css',
+	HTML: './dist',
+	ASSETS: {
+		FONTS: './dist/assets/fonts',
+		IMAGES: './dist/assets/images',
+		STYLE: './dist/assets/css',
 	}
 }
 
 // sass
 gulp.task('sass', () => {
-	return new Promise( resolve => {
+	return new Promise(resolve => {
 		var options = {
-			outputStyle : 'expanded', // nested, expanded, compact, compressed
-			indentType : 'tab',
-			indentSize : 4
+			outputStyle: 'expanded', // nested, expanded, compact, compressed
+			indentType: 'tab',
+			indentSize: 4
 		}
 
 		gulp.src(PATH.ASSETS.STYLE + '/*.scss')
-			.pipe( sass(options) )
-			.pipe( gulp.dest( DEST_PATH.ASSETS.STYLE) );
+			.pipe(sass(options))
+			.pipe(gulp.dest(DEST_PATH.ASSETS.STYLE));
 
 		resolve();
 	})
 });
+// nodemon
+gulp.task('nodemon:start', () => {
+	return new Promise(resolve => {
+		nodemon({script: 'app.js', watch: 'app'});
+		resolve();
+	});
+});
+// html
+gulp.task('html', () => {
+	return new Promise(resolve => {
+		gulp.src(PATH.HTML + '/*.html')
+			.pipe(gulp.dest(DEST_PATH.HTML));
+		resolve();
+	});
+});
+// watch
+gulp.task('watch', () => {
+	return new Promise(resolve => {
+		gulp.watch(PATH.HTML + "/**/*.html", gulp.series(['html']));
+		gulp.watch(PATH.ASSETS.STYLE + "/**/*.scss", gulp.series(['scss']));
+		resolve();
+	});
+});
+// browser sync
+gulp.task('browserSync', () => {
+	return new Promise(resolve => {
+		browserSync.init(null, {proxy: 'http://localhost:8005', port: 8006});
+		resolve();
+	});
+});
 
-gulp.task('default', gulp.series(['sass']));
+gulp.task('default', gulp.series(['sass', 'html', 'nodemon:start','browserSync','watch']));
