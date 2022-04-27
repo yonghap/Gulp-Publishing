17bc1c8399f6e864e217ba551100b68418350cf7
Park Yong Ha <yonghapk@gmail.com>
2022-04-28 00:10:19 +0900

    gulp add 4.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,37 @@
 const { series, parallel, src, dest } = require('gulp'),
 	sass = require('gulp-sass')(require('sass'));
 
-const CONFIG = {
-	language : ['ko'],
-	workspace : {
 
+/**
+ * 환경설정
+ */
+const CONFIG = {
+	language : ['KO','EN'],
+	workspace : {
+		HTML_KO : './workspace/html/html_ko',
+		HTML_EN : './workspace/html/html_en',
+		ASSETS : {
+			FONTS : './workspace/assets/fonts',
+			IMAGES : './workspace/assets/images',
+			STYLE : './workspace/assets/scss',
+			SCRIPT : './workspace/assets/script',
+			LIBRARY : './workspace/assets/lib'
+		}
 	},
 	deploy : {
-
+		HTML_KO: './dist/html_ko',
+		HTML_EN: './dist/html_en',
+		ASSETS: {
+			FONTS: './dist/assets/fonts',
+			IMAGES: './dist/assets/images',
+			STYLE: './dist/assets/css',
+			SCRIPT: './dist/assets/script',
+			LIB: './dist/assets/lib'
+		}
 	}
 }
 
-function sass(cb) {
+function complieCSS(cb) {
 	cb();
 }
 
@@ -19,9 +39,9 @@ function build(cb) {
 	cb();
 }
 
-const defaultTasks = {
-	sass
-}
+const defaultTasks = [
+	complieCSS
+]
 
 exports.build = build;
 exports.default = series(defaultTasks);
