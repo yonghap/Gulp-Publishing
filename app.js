e831f0be08b7d3142aa28101882c22722aa1c56c
퍼블리싱 <yonghap@bdot.co.kr>
2022-05-02 17:47:48 +0900

    Gulp for Publishing 05/02

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.set('views', '${ __dirname }/dist/');
+app.set('views', '${ __dirname }/dist');
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
-app.use('/', express.static(`${__dirname}/dist/`));
+app.use('/', express.static(`${__dirname}/dist`));
 app.get('/', (req, res) => {
-	console.log('test');
 	res.render('index', {});
 });
 var server = app.listen(8005, () => {
--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,57 @@
-const { series, parallel, src, dest } = require('gulp'),
+const {series, parallel, watch, src, dest} = require('gulp'),
 	sass = require('gulp-sass')(require('sass')),
 	ejs = require('gulp-ejs'),
-	nodemon = require('gulp-nodemon');
+	imagemin = require('gulp-imagemin'),
+	del = require('del'),
+	nodemon = require('gulp-nodemon'),
+	browserSync = require('browser-sync').create();
 
 /**
  * Config
  */
 const CONFIG = {
-	language : ['KO','EN'],
-	workspace : {
-		HTML_KO : './workspace/html/html_ko',
-		HTML_EN : './workspace/html/html_en',
-		ASSETS : {
-			FONTS : './workspace/assets/fonts',
-			IMAGES : './workspace/assets/images',
-			STYLE : './workspace/assets/scss',
-			SCRIPT : './workspace/assets/script',
-			LIBRARY : './workspace/assets/lib'
+	workspace: {
+		HTML: './workspace/html',
+		ASSETS: {
+			FONTS: './workspace/assets/fonts',
+			IMAGES: './workspace/assets/images',
+			STYLE: './workspace/assets/scss',
+			SCRIPT: './workspace/assets/script',
+			LIBRARY: './workspace/assets/library'
 		}
 	},
-	deploy : {
-		HTML_KO: './dist/html_ko',
-		HTML_EN: './dist/html_en',
+	deploy: {
+		HTML: './dist/html',
 		ASSETS: {
 			FONTS: './dist/assets/fonts',
 			IMAGES: './dist/assets/images',
 			STYLE: './dist/assets/css',
 			SCRIPT: './dist/assets/script',
-			LIB: './dist/assets/lib'
+			LIBRARY: './dist/assets/library'
 		}
 	}
 }
 
 /**
+ * @task : EJS
+ */
+function EJS() {
+	return new Promise(resolve => {
+		src([CONFIG.workspace.HTML + '/**/*.html', '!'+CONFIG.workspace.HTML + '/**/include/*.html'],)
+			.pipe(ejs())
+			.pipe(dest(CONFIG.deploy.HTML));
+		resolve();
+	});
+}
+
+/**
  * @task : Sass
  */
-function CSS() {
+function CompileCSS() {
 	return new Promise(resolve => {
 		src(CONFIG.workspace.ASSETS.STYLE + '/*.scss')
 			.pipe(sass({
-				outputStyle:  'compressed'
+				outputStyle: 'compressed'
 			}).on('error', sass.logError))
 			.pipe(dest(CONFIG.deploy.ASSETS.STYLE))
 		resolve();
@@ -47,9 +59,88 @@ function CSS() {
 }
 
 /**
+ * @task : Imagemin
+ */
+function Imagemin() {
+	return new Promise(resolve => {
+		src(CONFIG.workspace.ASSETS.IMAGES + '/*.*')
+			.pipe(imagemin([
+				imagemin.gifsicle({interlaced: false}),
+				imagemin.mozjpeg({quality: 95, progressive: false}),
+				imagemin.optipng({optimizationLevel: 5}),
+				imagemin.svgo({plugins: [{removeViewBox: true}, {cleanupIDs: false}]})]))
+			.pipe(dest(CONFIG.deploy.ASSETS.IMAGES));
+		resolve();
+	});
+}
+
+/**
+ * @task : Copy - Library
+ */
+function Library() {
+	return new Promise(resolve => {
+		src(CONFIG.workspace.ASSETS.LIBRARY + '/**/*')
+			.pipe(dest(CONFIG.deploy.ASSETS.LIBRARY));
+		resolve();
+	});
+}
+
+/**
+ * @task : Copy - Javascript
+ */
+function Script() {
+	return new Promise(resolve => {
+		src(CONFIG.workspace.ASSETS.SCRIPT + '/**/*')
+			.pipe(dest(CONFIG.deploy.ASSETS.SCRIPT));
+		resolve();
+	});
+}
+
+/**
+ * @task : Copy - Fonts
+ */
+function Font() {
+	return new Promise(resolve => {
+		src(CONFIG.workspace.ASSETS.FONTS + '/**/*')
+			.pipe(dest(CONFIG.deploy.ASSETS.FONTS));
+		resolve();
+	});
+}
+
+/**
+ * @task : Clean
+ */
+function Clean() {
+	return new Promise(resolve => {
+		del.sync('./dist');
+		resolve();
+	});
+}
+
+/**
+ * @task : Watch
+ */
+function Watch() {
+	watch(CONFIG.workspace.HTML + '/**/*.html', EJS);
+	watch(CONFIG.workspace.ASSETS.STYLE + '/**/*.scss', CompileCSS);
+	watch(CONFIG.workspace.ASSETS.IMAGES + "/*.*", Imagemin);
+	watch(CONFIG.workspace.ASSETS.SCRIPT + "/*.*", Script);
+}
+
+/**
+ * @task : Browser Sync
+ */
+function BrowserSync() {
+	return new Promise(resolve => {
+		browserSync.init({proxy: 'http://localhost:8005', port: 8006});
+		resolve();
+	});
+}
+
+/**
  * @task : Nodemon
  */
-function ServerStart(cb) {
+function ServerStart() {
 	return new Promise(resolve => {
 		nodemon({script: 'app.js', watch: 'app'});
 		resolve();
@@ -57,8 +148,18 @@ function ServerStart(cb) {
 }
 
 const defaultTasks = [
-	CSS
+	CompileCSS,
+	EJS,
+	Script,
+	Library,
+	Font,
+	Imagemin,
+	ServerStart,
+	BrowserSync,
+	Watch
 ]
 
-exports.build = build;
-exports.default = series(defaultTasks);
+module.exports = {
+	default : series(defaultTasks),
+	clean : series(Clean)
+}
